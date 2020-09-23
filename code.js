6c6d8a978f51c0059fa7b61e82b79e7ee49d8d33
Nahla-a <nahla.abuelela@gmail.com>
2020-09-23 02:08:10 -0500

    added expense-manager-app

---
+++ code.js
@@ -0,0 +1,73 @@
+const EXPENSE_KEY = "expenseList";
+
+let addItemButton = document.getElementById("addElementButton");
+addItemButton.addEventListener("click", function () {
+    let typeOfExpense = document.getElementById("typeBox");
+    let nameOfExpense = document.getElementById("nameBox");
+    let dateOfExpense = document.getElementById("dateBox");
+    let amountOfExpense = document.getElementById("amountBox");
+
+    if (typeOfExpense.value == "--") {
+        alert("plz select a proper type of expense")
+    }
+    else if (nameOfExpense.value == "") {
+        alert("plz enter a name of expense")
+    }
+    else if (dateOfExpense.value == "") {
+        alert("plz pick up a date from the calender")
+    }
+    else if (amountOfExpense.value == 0) {
+        alert("the amount should be greater than 0")
+    }
+    else {
+        deleteTempRow();
+        addExpenseToTable(typeOfExpense.value, nameOfExpense.value, dateOfExpense.value, amountOfExpense.value);
+        storeExpenseToLocalStorage(typeOfExpense, nameOfExpense, dateOfExpense, amountOfExpense);
+        clearInputText();
+
+    }
+
+
+})
+
+function addExpenseToTable(type, name, date, amount, remove) {
+    let table = document.getElementById("expenseTable");
+    let newRow = table.insertRow();
+    let cell1 = newRow.insertCell();
+    let cell2 = newRow.insertCell();
+    let cell3 = newRow.insertCell();
+    let cell4 = newRow.insertCell();
+    let cell5 = newRow.insertCell();
+
+    cell1.innerHTML = type;
+    cell2.innerHTML = name;
+    cell3.innerHTML = date;
+    cell4.innerHTML = amount;
+    cell5.innerHTML = remove;
+
+}
+
+function deleteTempRow() {
+    let tempRow = document.getElementById("tempRow");
+    if (tempRow != null) {
+        tempRow.parentNode.removeChild(tempRow);
+    }
+}
+
+function clearInputText() {
+    document.getElementById("typeBox").value = "";
+    document.getElementById("nameBox").value = "";
+    document.getElementById("dateBox").value = "";
+    document.getElementById("amountBox").value ="";
+}
+
+function storeExpenseToLocalStorage(typeOfExpense, nameOfExpense, dateOfExpense, amountOfExpense) {
+    let expenseLocalStorage = localStorage.getItem(EXPENSE_KEY);
+    if (expenseLocalStorage == null) {
+        expenseLocalStorage = [];
+    } else {
+        expenseLocalStorage = JSON.parse(expenseLocalStorage);
+    }
+    expenseLocalStorage.push({ theType: typeOfExpense.value }, { theName: nameOfExpense.value }, { theDate: dateOfExpense.value }, { theAmount: amountOfExpense.value })
+    localStorage.setItem(EXPENSE_KEY, JSON.stringify(expenseLocalStorage));
+}
